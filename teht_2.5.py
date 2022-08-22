7c64b47e90b73b326e466a0f49ce716f66a3d575
Eveliinakytovaara <eveliina.kytovaara@metropolia.fi>
2022-08-22 23:37:23 +0300

    eka commit

--- teht_2.5.py
+++ teht_2.5.py
@@ -5,3 +5,22 @@
 #Yksi leiviskä on 20 naulaa.
 #Yksi naula on 32 luotia.
 #Yksi luoti on 13,3 grammaa.
+
+
+#naulat grammoina = 32 * 13,3
+#leiviskät grammoina = 20*32*13,3g
+
+leiviska = int(input("anna leiviskän määrä "))
+naula =  int(input("anna naulan määrä "))
+luoti = int(input("anna luodin määrä "))
+
+#laskutoimitukset
+leiviska_gr = leiviska * 20 * 32 * 13,3
+naula_gr = naula * 32 * 13,3
+luoti_gr = luoti * 13,3
+
+
+#tulostukset
+print (f" {leiviska} leiviskää painaa {leiviska_gr} grammaa ")
+print (f" {naula} naulaa painaa {naula_gr} grammaa ")
+print (f" {luoti} luotia painaa {luoti_gr} grammaa ")
